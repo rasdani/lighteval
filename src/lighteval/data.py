e80f9d44a02547ebdfddbc3498fd54afaad1d551
rasdani <73563550+rasdani@users.noreply.github.com>
2024-10-25 22:19:03 +0000

    correct spelling of type variable import from torch

--- src/lighteval/data.py
+++ src/lighteval/data.py
@@ -24,7 +24,7 @@ from typing import Iterator
 
 import torch
 from torch.utils.data import Dataset
-from torch.utils.data.distributed import DistributedSampler, T_co
+from torch.utils.data.distributed import DistributedSampler, _T_co
 
 from lighteval.logging.hierarchical_logger import hlog_warn
 from lighteval.tasks.requests import (
@@ -304,7 +304,7 @@ class GenDistributedSampler(DistributedSampler):
     as our samples are sorted by length.
     """
 
-    def __iter__(self) -> Iterator[T_co]:
+    def __iter__(self) -> Iterator[_T_co]:
         if self.shuffle:
             # deterministically shuffle based on epoch and seed
             g = torch.Generator()
